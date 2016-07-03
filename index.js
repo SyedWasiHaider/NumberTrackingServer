05c28b112b07948de63ae64594517d352aca433c
SyedWasiHaider <wasih@wasih-mac.local>
2016-07-02 18:47:01 -0700

    Added file uploading

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var multer  =   require('multer');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
 });
 
-app.get('/cool', function(request, response){
-  response.send(cool());
+var upload = multer({ storage : storage}).single('userPhoto');
+
+
+app.get('/', function(request, response) {
+  response.send("hellooooo");
+});
+
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
